825b4f0b95d3d51e0d0e14e23e3f1fcfc43d8620
prasanganath <sandun.wed@gmail.com>
2021-01-27 08:14:11 +0530

    add docker file

--- springboot-docker/src/main/java/com/javatechie/spring/boot/docker/demo/SpringbootDockerApplication.java
+++ springboot-docker/src/main/java/com/javatechie/spring/boot/docker/demo/SpringbootDockerApplication.java
@@ -2,9 +2,18 @@ package com.javatechie.spring.boot.docker.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDockerApplication {
+	
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to JavaTechie..!!!!!!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerApplication.class, args);
