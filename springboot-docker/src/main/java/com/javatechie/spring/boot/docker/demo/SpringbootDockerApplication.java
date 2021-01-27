27459c4f2ef47219926ee135a1d97efabcee9968
prasanganath <sandun.wed@gmail.com>
2021-01-27 07:46:09 +0530

    docker

---
+++ springboot-docker/src/main/java/com/javatechie/spring/boot/docker/demo/SpringbootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerApplication.class, args);
+	}
+
+}
---
+++ springboot-docker/src/test/java/com/javatechie/spring/boot/docker/demo/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
